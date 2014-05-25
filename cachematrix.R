bd01dd527ff22f9a6b80207a11c8c31982fe7979
Sharma Kunapalli <Sharma.Kunapalli@gmail.com>
2014-05-24 20:07:17 -0700

    matrix project

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Example: Caching the inverse of a matrix
+## In this program we use the <<- operator which can be used to 
+## assign a value to an object in an environment that is different 
+## from the current environment. Below are two functions that are used 
+## to create a special object that stores a matrix and cache's its matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes in a matrix and returns a new kind of matrix, 
+## what we call a cached matrix, which is really a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the vector
+## 3. set the value of the mean
+## 4. get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        m <- NULL ## 
         set <- function(y) {
                 x <<- y
                 m <<- NULL
@@ -16,7 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
+##The following function calculates the matrix of the special "matrix" created 
+##with the above function. However, it first checks to see if the matrix has 
+##already been calculated. If so, it gets the matrix from the cache and 
+##skips the computation. Otherwise, it calculates the matrix of the data and 
+## sets the value of the matrix in the cache via the setmatrix function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
